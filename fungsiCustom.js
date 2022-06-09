06fb512116eb12c6d8770efbb4e628f504225d77
alamkamajana <alamkamajana@gmail.com>
2022-06-09 15:44:55 +0800

    First Commit

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+var fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,34 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let arrJSON = [];
+  fs.readFile(file1, function(err, data) {
+    let dataJSON = JSON.parse(data);
+    textSplited = dataJSON.message.split(' ')
+    // console.log(textSplited)
+    arrJSON.push(textSplited[1])
+    // fnCallback(arrJSON)
+  });
+  fs.readFile(file2, function(err, data) {
+    let dataJSON2 = JSON.parse(data);
+    textSplited2 = dataJSON2[0].message.split(' ')
+    arrJSON.push(textSplited2[1])
+    // textSplited = dataJSON.message.split(' ')
+    // console.log(dataJSON2[0])
+    // arrJSON.push(textSplited[1])
+    // fnCallback(arrJSON)
+  });
+  fs.readFile(file3, function(err, data) {
+    let dataJSON3 = JSON.parse(data);
+    textSplited3 = dataJSON3[0]["data"]["message"].split(' ')
+    arrJSON.push(textSplited3[1])
+    // console.log(dataJSON3[0]["data"]["message"])
+    fnCallback(arrJSON)
+  });
+  // console.log(fnCallback(arrJSON))
+  
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
